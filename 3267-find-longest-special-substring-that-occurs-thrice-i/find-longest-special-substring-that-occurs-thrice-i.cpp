f7c5b474a4edb9a0868ffe07037cd50e831dd537
Nitin Tak <129604612+nitintak13@users.noreply.github.com>
2025-10-03 12:28:28 +0530

    Time: 10 ms (60.67%) | Memory: 15.2 MB (23.14%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     int maximumLength(string s) {
         int n = s.size();
-        unordered_map<string, int> mp;
-        for (int i = 0; i < n; i++) {
-            string str = "";
-            for (int j = i; j < n; j++) {
-                if (s[i] != s[j])
+        vector<vector<int>> mat(26, vector<int>(n + 1, 0));
+        int l = 0, i = 0, prev = s[0];
+        int ans = -1;
+        while (i < n) {
+            int curr = s[i];
+            if (curr == prev) {
+                l++;
+                mat[s[i] - 'a'][l]++;
+            } else {
+                l = 1;
+                mat[s[i] - 'a'][l]++;
+                prev = curr;
+            }
+            i++;
+        }
+        for (int row = 0; row < 26; row++) {
+            int sum = 0;
+            for (int col = n; col >= 1; col--) {
+                sum += mat[row][col];
+                if (sum >= 3) {
+                    ans = max(ans, col);
                     break;
-                str += s[i];
-                mp[str]++;
+                }
             }
         }
-        int maxi = -1, len = -1;
-        for (auto it : mp) {
-            string p = it.first;
-            int k = p.size();
-            if (it.second >= 3 && len < k) {
-                maxi = max(maxi, it.second);
-                len = k;
-            }
-        }
-        return len;
+        return ans;
     }
 };
